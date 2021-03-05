3b37ab01020224d2883ce840490ff4656e12abba
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2021-03-05 15:11:26 -0700

    cards and card item started

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../../App.css';
 import ShowCase from '../../components/ShowCase';
+import Cards from '../Cards';
 
 
 const Home = () => {
     return (
         <>
             <ShowCase />
+            <Cards />
         </>
     )
 }
